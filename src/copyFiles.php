cb87e12958a06fb7d01233243618af1d01d26091
Zarko Atanasov <zarko.atanasov@gmail.com>
2018-07-20 21:47:46 +0300

    Changes in post-script code

--- src/copyFiles.php
+++ src/copyFiles.php
@@ -1,10 +1,18 @@
 <?php
 
 
-$current_dir = basename(dirname(__FILE__));
+$current_dir = basename(dirname(__FILE__)) . '/';
 $remote_dir = './application';
 
-print($current_dir);
+$config_files = ['aliases','except_fields','rules'];
+$libraries = ['FVR'];
 
+foreach ($config_files as $key => $file) {
+    copy($current_dir . 'config/'.$file.'.php', './application/config/'.$file.'.php');
+}
+
+foreach ($libraries as $key => $file) {
+    copy($current_dir . 'libraries/'.$file.'.php', './application/config/'.ucfirst($file).'.php');
+}
 
 
