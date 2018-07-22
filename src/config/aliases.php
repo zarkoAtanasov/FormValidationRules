08968b20905c6bc27ca4df495da6c7e9b87f7a76
Zarko Atanasov <zarko.atanasov@gmail.com>
2018-07-22 16:16:05 +0300

    Added alias for repeat password

--- src/config/aliases.php
+++ src/config/aliases.php
@@ -18,7 +18,7 @@ return [
     'firstname'=>'first_name',
     'lastname'=>'last_name|family_name',
     'email'=>'mail|email_address|emailaddress',
-    'repeat_email'=>'repeatmail|repeat_mail|email2|mail2',
+    'repeat_email'=>'repeatmail|repeat_mail|email2|mail2|passconf',
     'phone'=>'telephone|mobile|phone_number|phonenumber',
     'password'=>'pass',
     'repeat_password'=>'repeatpassword|repeat_pass|password2',
